3785ed54fe1576c89b7324d7fe493478f9fc57bc
bk7191 <133879921+bk7191@users.noreply.github.com>
2024-03-16 10:18:11 +0100

    Delete Le_site/LITRevu directory

--- Le_site/LITRevu/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AbstractUser, Ticket, Review, UserFollows
-# Register your models here.
-
-admin.site.register(Ticket)
-admin.site.register(Review)
-admin.site.register(UserFollows)
