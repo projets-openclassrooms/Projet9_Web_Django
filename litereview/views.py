9bb08989ada637fcac1e82a42ed3b84466231bd5
bk7191 <bk7191014@gmail.com>
2024-04-12 21:42:41 +0200

    page de flux des tickets et revues / creation de revue sur 1 ticket

--- litereview/views.py
+++ litereview/views.py
@@ -287,24 +287,26 @@ def posts_page(request):
 @login_required
 def follower_page(request):
     follow_form = forms.FollowForm()
+    # les abonnements
     followed_user = UserFollows.objects.filter(user=request.user)
+    # les abonnes
     followers = UserFollows.objects.filter(followed_user=request.user)
     display_error = None
+    print("here")
     if request.method == "POST":
         post_value = request.POST.get("post_value")
+        print("-1")
 
         # checks the value sent by the post request
         if post_value == "follow":
-            # follows new user
-            follow_form = forms.FollowForm(request.POST.copy())
-            follow_form.data["followed_user"] = request.POST.get("user_to_follow")
-
-            # check if user inputs exists
+            print("0")
+            # recuperation de username et creation d l abonnement
             try:
                 followed_user_target = User.objects.get(
-                    username=request.POST.get("user_to_follow")
+                    username=request.POST.get("username")
                 )
-            except User:
+
+            except Exception:
                 display_error = "Nom d'utilisateur invalide"
                 context = {
                     "follow_form": follow_form,
@@ -314,18 +316,18 @@ def follower_page(request):
                 }
                 return render(request, "litereview/abonnement.html", context=context)
 
-            follow_form.followed_user = followed_user_target
-            if any([follow_form.is_valid()]):
-                if followed_user_target == request.user:
-                    display_error = "Vous ne pouvez pas vous abonner à vous même."
-                else:
-                    try:
-                        follow = follow_form.save(commit=False)
-                        follow.user = request.user
-                        follow.followed_user = followed_user_target
-                        follow.save()
-                    except IntegrityError:
-                        display_error = "Vous êtes déjà abonné à cet utilisateur"
+            print("1")
+            if followed_user_target == request.user:
+                display_error = "Vous ne pouvez pas vous abonner à vous même."
+            else:
+                try:
+                    print("2")
+                    follow = UserFollows()
+                    follow.user = request.user
+                    follow.followed_user = followed_user_target
+                    follow.save()
+                except IntegrityError:
+                    display_error = "Vous êtes déjà abonné à cet utilisateur"
         else:
             # deletes link with followed_user having 'post_value' username
             unfollow = UserFollows.objects.get(
